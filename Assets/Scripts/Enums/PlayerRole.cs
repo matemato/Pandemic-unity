3c6ba174018f5a3b6a7031e48494ce34cee4fa5d
matemato <47794629+matemato@users.noreply.github.com>
2023-09-06 23:55:08 +0200

    PlayerRole enums

---
+++ Assets/Scripts/Enums/PlayerRole.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerRole
+{
+    ROLE_PLANNER,
+    ROLE_DISPATCHER,
+    ROLE_MEDIC,
+    ROLE_EXPERT,
+    ROLE_SPECIALIST,
+    ROLE_RESEARCHER,
+    ROLE_SCIENTIST,
+    NUM_ROLES
+}
+
