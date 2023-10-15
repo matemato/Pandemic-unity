5047f80c069de8f9b714501597aa2169380cc022
ar43 <47793453+ar43@users.noreply.github.com>
2023-10-16 00:04:15 +0200

    Added InfectionType enum

---
+++ Assets/Scripts/Enums/InfectionType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InfectionType
+{
+    VIRUS_BLUE,
+    VIRUS_RED,
+    VIRUS_YELLOW,
+    VIRUS_BLACK,
+    EXPLOSION
+}
