ea15aa9718c09e964cc3cae76ae30bcf18b6c87a
matemato <47794629+matemato@users.noreply.github.com>
2025-12-05 23:16:40 +0100

    Johannesburg fix

--- Assets/Scripts/UI/PlayerHandManager.cs
+++ Assets/Scripts/UI/PlayerHandManager.cs
@@ -123,7 +123,7 @@ public class PlayerHandManager : MonoBehaviour
              
         if (playerId == id)
         {
-            var newCard = Instantiate(_playerCardPrefab, new Vector3(-770 + (160 * TotalCardCount), 0, -1), Quaternion.identity);
+            var newCard = Instantiate(_playerCardPrefab, new Vector3(-740, 250, -1), Quaternion.identity);
             newCard.transform.SetParent(gameObject.transform, false);
             newCard = newCard.transform.GetChild(0).gameObject;     
             newCard.GetComponent<PlayerCardScript>().SetPlayerCard(playerCard);
@@ -140,7 +140,7 @@ public class PlayerHandManager : MonoBehaviour
                     break;
                 }
             }
-            // ReorderPlayerHand();
+            ReorderPlayerHand();
 			if(TotalCardCount > 7)
 			{
 				var console = GameObject.FindGameObjectWithTag("Console").GetComponent<Console>();
